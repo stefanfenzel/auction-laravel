18a5bac8e2744da0ab610b26c2db9f1872f58227
Stefan Fenzel <fenzel@mehrkanal.com>
2024-11-27 13:21:21 +0100

    Install breeze and add authentication

---
+++ app/Http/Auth/Controllers/AuthenticatedSessionController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Gurulabs\Http\Auth\Controllers;
+
+use Gurulabs\App\Controllers\Controller;
+use Gurulabs\Http\Auth\Requests\LoginRequest;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
+
+class AuthenticatedSessionController extends Controller
+{
+    /**
+     * Display the login view.
+     */
+    public function create(): View
+    {
+        return view('auth.login');
+    }
+
+    /**
+     * Handle an incoming authentication request.
+     */
+    public function store(LoginRequest $request): RedirectResponse
+    {
+        $request->authenticate();
+
+        $request->session()->regenerate();
+
+        return redirect()->intended(route('dashboard', absolute: false));
+    }
+
+    /**
+     * Destroy an authenticated session.
+     */
+    public function destroy(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
+}
---
+++ app/Http/Auth/Controllers/EmailVerificationPromptController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Gurulabs\Http\Auth\Controllers;
+
+use Gurulabs\App\Controllers\Controller;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class EmailVerificationPromptController extends Controller
+{
+    /**
+     * Display the email verification prompt.
+     */
+    public function __invoke(Request $request): RedirectResponse|View
+    {
+        return $request->user()->hasVerifiedEmail()
+                    ? redirect()->intended(route('dashboard', absolute: false))
+                    : view('auth.verify-email');
+    }
+}
